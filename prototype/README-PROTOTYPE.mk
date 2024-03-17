20db17ea78b8d7bf5967ef66490d5d96b7ab28cf
ryorn-man <158655846+ryorn-man@users.noreply.github.com>
2024-03-16 23:44:39 -0400

    Update and rename readme-prototype.mk to README-PROTOTYPE.mk

---
+++ prototype/README-PROTOTYPE.mk
@@ -0,0 +1,4 @@
+# Nintendo Database – Prototype 
+
+## Overview
+In this stage, the Nintendo Database web application exists in an unfinished form, and is connected to an early version of the database for the website.
